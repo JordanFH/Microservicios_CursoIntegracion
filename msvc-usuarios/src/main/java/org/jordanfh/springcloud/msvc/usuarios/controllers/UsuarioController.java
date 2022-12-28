7aefc4fabec1dfceceb96707c41c3d640599bad4
JordanFH <jfernandezh19_2@unc.edu.pe>
2022-12-28 14:59:39 -0500

    Versión 1.0

--- msvc-usuarios/src/main/java/org/jordanfh/springcloud/msvc/usuarios/controllers/UsuarioController.java
+++ msvc-usuarios/src/main/java/org/jordanfh/springcloud/msvc/usuarios/controllers/UsuarioController.java
@@ -22,18 +22,41 @@ public class UsuarioController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> detalle(@PathVariable long id) {
+    public ResponseEntity<?> detalle(@PathVariable Long id) {
         Optional<Usuario> optional = service.buscarId(id);
         if (optional.isPresent()) {
             return ResponseEntity.ok(optional.get());
-        } else {
-            return ResponseEntity.notFound().build();
         }
+        return ResponseEntity.notFound().build();
+
     }
 
     @PostMapping
-    public ResponseEntity<?> crear(@RequestBody Usuario usuario){
+    public ResponseEntity<?> crear(@RequestBody Usuario usuario) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(usuario));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(Usuario usuario, long id) {
+        Optional<Usuario> optional = service.buscarId(id);
+        if (optional.isPresent()) {
+            Usuario usuarioDB = optional.get();
+            usuarioDB.setNombre(usuario.getNombre());
+            usuarioDB.setEmail(usuario.getEmail());
+            usuarioDB.setPassword(usuario.getPassword());
+            return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(usuario));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(long id) {
+        Optional<Usuario> optional = service.buscarId(id);
+        if (optional.isPresent()) {
+            service.eliminar(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
